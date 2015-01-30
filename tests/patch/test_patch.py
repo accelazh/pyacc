d483416d815eda07c7f5a63d812bcf3fcf22cbe5
Accela Zhao <accelazh@gmail.com>
2015-01-30 22:26:19 +0800

    fix incorrect path

--- tests/patch/test_patch.py
+++ tests/patch/test_patch.py
@@ -1,3 +1,6 @@
+# Manual tests. Tested class method, module method and decorators. The argument
+# decorate=True/False and return_value=XXX is tested.
+
 from pyacc.tests.patch.mda.cls_a import A
 from pyacc.common.patch import Patch
 
@@ -7,42 +10,42 @@ a.f1()
 
 print '--------decorate----------'
 
-with Patch('patch.mdb.cls_b.B.f2') as mock:
+with Patch('pyacc.tests.patch.mdb.cls_b.B.f2') as mock:
     a = A()
     a.f1()
     print 'mock trace %s' % mock
 
 print '----------break-----------'
 
-with Patch('patch.mdb.cls_b.B.f2', decorate=False) as mock:
+with Patch('pyacc.tests.patch.mdb.cls_b.B.f2', decorate=False) as mock:
     a = A()
     a.f1()
     print 'mock trace %s' % mock
 
 print '----------return val-----------'
 
-with Patch('patch.mdb.cls_b.B.f2', decorate=False, return_value=7) as mock:
+with Patch('pyacc.tests.patch.mdb.cls_b.B.f2', decorate=False, return_value=7) as mock:
     a = A()
     a.f1()
     print 'mock trace %s' % mock
 
 print '--------decorate C----------'
 
-with Patch('patch.mdc.cls_c.C') as mock:
+with Patch('pyacc.tests.patch.mdc.cls_c.C') as mock:
     a = A()
     a.f1()
     print 'mock trace %s' % mock
 
 print '----------break C-----------'
 
-with Patch('patch.mdc.cls_c.C', decorate=False) as mock:
+with Patch('pyacc.tests.patch.mdc.cls_c.C', decorate=False) as mock:
     a = A()
     a.f1()
     print 'mock trace %s' % mock
 
 print '----------return val C-----------'
 
-with Patch('patch.mdc.cls_c.C', decorate=False, return_value=7) as mock:
+with Patch('pyacc.tests.patch.mdc.cls_c.C', decorate=False, return_value=7) as mock:
     a = A()
     a.f1()
     print 'mock trace %s' % mock
